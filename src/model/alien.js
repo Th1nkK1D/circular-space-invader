0ebd8dbe778920ef8b4cc55a8141ac293a9547cd
Th1nkK1D <witheep@gmail.com>
2022-10-08 20:15:38 +0300

    feat: Add new alien body

--- src/model/alien.js
+++ src/model/alien.js
@@ -6,10 +6,43 @@ class Alien extends Fighter {
   }
 
   drawBody() {
-    fill(255);
-    noStroke();
+    const width = this.radius * 2;
+    const height = (this.radius * 4) / 3;
+    const dotSize = this.radius / 3;
 
-    circle(0, -this.orbitalRadius, this.radius * 2);
+    const yOffset = -this.orbitalRadius;
+
+    noStroke();
+    fill(255);
+
+    // body
+    rect(-width / 2, -height / 2 + yOffset, width, height);
+
+    // arms
+    rect(-width / 2 - dotSize, -dotSize / 2 + yOffset, dotSize, dotSize);
+    rect(width / 2, -dotSize / 2 + yOffset, dotSize, dotSize);
+
+    // legs
+    rect((dotSize - width) / 2, height / 2 + yOffset, dotSize, dotSize);
+    rect(width / 2 - 2 * dotSize, height / 2 + yOffset, dotSize, dotSize);
+
+    // hat
+    rect(-dotSize / 2, -height / 2 - dotSize + yOffset, dotSize, dotSize);
+
+    // eyes
+    fill(0);
+    rect(
+      -width / 2 + dotSize,
+      -height / 2 + dotSize + yOffset,
+      dotSize,
+      dotSize
+    );
+    rect(
+      width / 2 - 2 * dotSize,
+      -height / 2 + dotSize + yOffset,
+      dotSize,
+      dotSize
+    );
   }
 
   updateAngle() {
